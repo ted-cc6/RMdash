00b6eec664fb71339b6b8ef613ad843838e205dd
georgiepowell1 <61029305+georgiepowell1@users.noreply.github.com>
2020-02-14 12:00:45 +0000

    updates to the dashboard to show  the events and notifications

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,22 +1,104 @@
-import React, { Component } from 'react';
-
-/*Native base theme*/
-import { Container, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
-
-import { createStackNavigator } from '@react-navigation/stack';
-
-const Stack = createStackNavigator();
-
-export default class Dashboard extends Component {
+import React, {Component} from 'react';
+import {Image, StatusBar} from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  Card,
+  CardItem,
+  Text,
+  Body,
+  Left,
+  Button,
+  Icon,
+  Title,
+  Thumbnail,
+  Right,
+} from 'native-base';
+export default class UpcomingEvents extends Component {
   render() {
     return (
-        <Container>
-            <Content>
-              <Text>
-                Dashboard goes here
+      <Container>
+        <Header style={{backgroundColor: '#103662'}}>
+          <StatusBar backgroundColor="#103662" />
+          <Left>
+            <Button transparent>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body style={{flex: 2}}>
+            <Title>Home</Title>
+          </Body>
+        </Header>
+        <Content padder>
+          <Card style={{borderRadius: 8}}>
+            <CardItem
+              header
+              bordered
+              style={{borderTopLeftRadius: 8, borderTopRightRadius: 8}}>
+              <Text
+                style={{fontSize: 20, fontWeight: 'bold', color: '#103662'}}>
+                UPCOMING EVENTS
               </Text>
-            </Content>
-        </Container>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button backgroundColor="#103662">
+                  <Text> 09{'\n'}APR</Text>
+                </Button>
+                <Body>
+                  <Text>Meeting with Elon</Text>
+                  <Text>Credit Suisse Office</Text>
+                  <Text>5pm - 7pm</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button backgroundColor="#103662">
+                  <Text> 10{'\n'}JUN</Text>
+                </Button>
+                <Body>
+                  <Text>Product Training</Text>
+                  <Text>8am - 10am</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem
+              bordered
+              style={{
+                borderBottomLeftRadius: 8,
+                borderBottomRightRadius: 8,
+              }}></CardItem>
+          </Card>
+          <Card style={{borderRadius: 8}}>
+            <CardItem
+              header
+              bordered
+              style={{borderTopLeftRadius: 8, borderTopRightRadius: 8}}>
+              <Text
+                style={{fontSize: 20, fontWeight: 'bold', color: '#103662'}}>
+                NOTIFICATIONS
+              </Text>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text style={{fontSize: 25}}>- EXT.1 up by 0.8%</Text>
+                <Text style={{fontSize: 25}}>
+                  - FTSE down by 1.7%{' '}
+                  <Icon name="alert" style={{fontSize: 30, color: 'red'}} />
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem
+              bordered
+              style={{
+                borderBottomLeftRadius: 8,
+                borderBottomRightRadius: 8,
+              }}></CardItem>
+          </Card>
+        </Content>
+      </Container>
     );
   }
-};
+}
